8e80bf9a921fd2c6bca82d520e650e71f73e4c53
curd4 <93422010+curd4@users.noreply.github.com>
2021-11-06 10:12:56 +0200

    add examples
    add examples

--- StringExample/src/stringexample/StringExample.java
+++ StringExample/src/stringexample/StringExample.java
@@ -13,8 +13,35 @@ public class StringExample {
         
         //String name = input.nextLine();
         String name = input.next();
-        String name = input.next("");
+        //String name = input.next("");
+        
         System.out.println("ну, привет, " + name);
+        
+        int n = name.length();
+        int numLastChar = n -1;
+        for(int i = numLastChar; i >= 0; i--)
+        {
+            System.out.println(name.charAt(i));
+        }
+        if (n >= 4)
+        {
+            String newString = name.substring(1,3 + 1);
+            System.out.println(newString);
+        }
+        
+        String[] words = name.split(" ");
+        int M = words.length;
+        String firstName = words[0];
+        String lastName = words[1];
+        
+        System.out.printf("Вы ввели слов %d, это имя = %s и фамилия = %s", M, firstName, lastName);
+        
+        boolean result = name.contains(lastName);
+        if (result == true)
+        {
+            System.out.println("строка = "+name+" содержит строку "+lastName);
+            
+        }
     }
     
 }
