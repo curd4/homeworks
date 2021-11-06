2ce48e3d476126cbe1a861caecf6d99a9f83d8cb
curd4 <93422010+curd4@users.noreply.github.com>
2021-11-06 10:33:46 +0200

    my library
    my library

---
+++ MyJavaLibrary/src/math/MyMath.java
@@ -0,0 +1,13 @@
+
+package math;
+
+
+public class MyMath {
+    
+    public static double calcCircleArea(double r)
+    {
+        double Area = Math.PI * r * r;
+        return Area;
+    }
+    
+}
---
+++ MyMathFunctionsTester/src/mymathfunctionstester/MyMathFunctionsTester.java
@@ -0,0 +1,19 @@
+
+package mymathfunctionstester;
+
+import math.MyMath;
+
+
+public class MyMathFunctionsTester {
+    
+    
+  
+    public static void main(String[] args) {
+        
+        double R =3;
+        double S = MyMath.calcCircleArea(R);
+        System.out.println("площадь круга с радиусом "+R+" равна "+S);
+      
+    }
+    
+}
